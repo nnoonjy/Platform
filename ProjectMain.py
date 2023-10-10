16da520297f895555cf728901194b2ad4b1a59fe
문정윤 <halfmoonjy99@pusan.ac.kr>
2023-10-10 10:48:48 +0900

    오류수정, 추가 기능

--- ProjectMain.py
+++ ProjectMain.py
@@ -10,7 +10,7 @@ class carInfo:
     def __init__(self):
         self.id = id
 
-    def newInfo(self, id, password):
+    def newInfo(self, id, type):
         self.id = id
         self.type = type
 
@@ -21,16 +21,42 @@ def customerMode():
     if carID in personalInfo:
         pass
     else:
+        newCarInfo = []
+        parkableSpace = []
+
         carType = int(
             input(
-                "Enter your car type ( 1: General Car, 2: Electric Car, 3: Motorcycle, 4: Truck )"
+                "Enter your car type ( 1: General Car, 2: Electric Car, 3: Motorcycle, 4: Truck ) : "
             )
         )
-        parkableSpace = []
-        for i in range(5):
-            parkableSpace.append(leftSpace[i][carType])  # 층수별로 남은 공간 리스트에 저장
 
-        personalInfo[carID] = carType  # 새로운 차 저장
+        newCarInfo.append(carType)
+
+        for i in range(5):
+            parkableSpace.append(leftSpace[i][carType - 1])  # 층수별로 남은 공간 리스트에 저장
+
+        # - todo. 층수별로 남은 갯수 안내
+
+        floor = True
+        chooseFloor = int(input("Choose the floor (1~5) : "))
+        print(chooseFloor)
+        while floor:
+            if 1 <= chooseFloor <= 5:
+                floor = False
+                space = True
+                while space:
+                    if leftSpace[chooseFloor - 1][carType - 1] != 0:
+                        space = False
+                        print("Welcome !")
+                        newCarInfo.append(chooseFloor)
+                        personalInfo[carID] = newCarInfo  # 새로운 차 저장
+                    else:
+                        chooseFloor = int(
+                            input("No space available on this floor. Choose again : ")
+                        )
+
+            else:
+                chooseFloor = int(input("Wrong floor. Choose again (1~5) : "))
 
 
 def adminMode():
@@ -38,17 +64,15 @@ def adminMode():
 
 
 if __name__ == "__main__":
-    isNotGood = True
-
-    while isNotGood:
+    while True:
         mode = input("Enter the mode ( 1: Customer mode, 2: Admin mode ) : ")
         if mode == "1":
-            isNotGood = False
             customerMode()
         elif mode == "2":
             adminPass = input("Enter the password : ")
             if personalInfo["Admin"] == adminPass:
-                isNotGood = False
                 adminMode()
             else:
                 print("Wrong password. Please Start from the beginning.")
+
+        print(personalInfo)
